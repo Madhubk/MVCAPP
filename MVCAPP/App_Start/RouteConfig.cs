357f91955268cff41c5556ce502166eaa6340ce6
Madhu <madhucseking@gmail.com>
2019-03-21 19:01:58 +0530

    first commit

--- MVCAPP/App_Start/RouteConfig.cs
+++ MVCAPP/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace MVCAPP
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             );
         }
     }
